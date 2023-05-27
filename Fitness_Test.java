4f3a679a283cc03f0fc0acba47a59600e92de2ab
Adarsh <adarshkumarrishu2002@gmail.com>
2023-05-27 14:40:45 +0530

    Insert

---
+++ Fitness_Test.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Fitness_Test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
--- Sunday_Count.java
+++ Sunday_Count.java
@@ -1,23 +1,27 @@
 import java.util.Scanner;
 
 public class Sunday_Count {
+    public static int firstSunday(String day) {
+        String arr[] = { "mon", "tue,", "wed", "thu", "fri", "sat", "sun" };
+        // int res=6;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].equals(day))
+                return 6 - i;
+        }
+        return 0;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the day of the month starting (mon , tue , wed , thu , fri , sat , sun): ");
         String str = sc.next();
         System.out.print("Enter the number of day's: ");
         int n = sc.nextInt();
-        String arr[] = { "mon", "tue,", "wed", "thu", "fri", "sat", "sun" };
 
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i].equals(str))
-                break;
-            int res = 1;
-            int rem = 6 - i;
-            n = n - rem;
-            if (n > 0)
-                res += n / 7;
-            System.out.println(res);
-        }
+        n = n - firstSunday(str);
+        if (n >= 0)
+            System.out.println((n / 7) + 1);
+        else
+            System.out.println(0);
     }
 }
